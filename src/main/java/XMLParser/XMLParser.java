8fcf723bc5f163c6c024a2538b7ac946fed1331e
John Yacoub <jyacoub@my.yorku.ca>
2022-02-07 12:03:46 -0500

    XML Parsing Trial and Error

--- src/main/java/XMLParser/XMLParser.java
+++ src/main/java/XMLParser/XMLParser.java
@@ -1,18 +1,19 @@
 package XMLParser;
 
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
 import java.io.StringReader;
 
 
@@ -24,48 +25,146 @@ import java.io.StringReader;
  * */
  public class XMLParser {
 	 
-	private static String xml = "";
-	private static Document doc ;
+	//private static String xml = "";
+	//private static Document doc ;
 
 	public static void getXml(Document doc) {
 		
-		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        try {
-            DocumentBuilder builder = factory.newDocumentBuilder();
-
-            // Get Document
-            Document document = doc;
-
+ 
+		
             // Normalize the xml structure
-            document.getDocumentElement().normalize();
+            doc.getDocumentElement().normalize();
+            
+            
+//            // Get all the element by the tag name
+//            NodeList scorePartwise = doc.getElementsByTagName("score-partwise");
+//            
+//            NodeList scoreDetails =  scorePartwise.getChildNodes();
+//            //Node Detail
+//            
+//            //NodeList scorePartwise = (NodeList) listObject.item(0);
+//            Node indentification = scorePartwise.item(0);
+//            //Node creator = indentification.item(0);
+//            		
+//            Element creatorType = (Element) indentification;
+//            System.out.println(creatorType); 
+//            		
+//            for(int j = 0; j < laptopDetails.getLength(); j++){
+//              Node detail = laptopDetails.item(j);
+//              if(detail.getNodeType() == Node.ELEMENT_NODE) {
+//                  Element detailElement = (Element) detail;
+//                  System.out.println("     " + detailElement.getTagName() + ": " + detailElement.getAttribute("value"));
+//              }
+//            }
+		
+            
+//            	NodeList scoreParts = doc.getElementsByTagName("score-partwise");
+//            	
+//            	for(int i = 0; i < scoreParts.getLength(); i++) {
+//            		System.out.println(scoreParts.item(i));
+//            	}
+//            	
+//            	//   NodeList identification = scoreParts.;
+//                Node creator = identification.getFirstChild();
+//                System.out.println("Creator is "  + creator.getTextContent());
+                
+                
+//                NodeList idenList = scoreParts.getChildNodes();
+//                Node iden = idenList.item(1);
+//                
+//                
+//                NodeList creatorList = iden.getChildNodes();
+//                Node creator = creatorList.item(1);
+//              
+//                
+//                
+//                if(creator.getNodeType() == Node.ELEMENT_NODE) {
+//                    Element detailElement = (Element) creator;
+//                    System.out.println("     " + detailElement.getTagName() + ": " + detailElement.getAttribute("value"));
+//                }
+//                
+            
+            
+//          
+//          if(scoreParts.getNodeType() == Node.ELEMENT_NODE) {
+//
+//
+//              NodeList laptopDetails =  scoreParts.getChildNodes();
+//              
+//              for(int j = 0; j < laptopDetails.getLength(); j++){
+//                  Node detail = laptopDetails.item(j);
+//                  if(detail.getNodeType() == Node.ELEMENT_NODE) {
+//                      Element detailElement = (Element) detail;
+//                      System.out.println("     " + detailElement.getTagName() + ": " + j);
+//                  }
+//
+//
+//          } 
+//      } 
+            
+            	NodeList scorePart = doc.getElementsByTagName("score-part");
+                System.out.println("Number of score parts: " + scorePart.getLength());
+                
+                Node partName = scorePart.item(0);
+                System.out.println(partName.ELEMENT_NODE);
 
-            // Get all the element by the tag name
-
-            NodeList laptopList = document.getElementsByTagName("score-partwise");
-            for(int i = 0; i <laptopList.getLength(); i++) {
-                Node laptop = laptopList.item(i);
-                if(laptop.getNodeType() == Node.ELEMENT_NODE) {
-
-                    Element laptopElement = (Element) laptop;
-                    System.out.println("Laptop Name: " + laptopElement.getAttribute("name"));
-
-                    NodeList laptopDetails =  laptop.getChildNodes();
-                    for(int j = 0; j < laptopDetails.getLength(); j++){
-                        Node detail = laptopDetails.item(j);
-                        if(detail.getNodeType() == Node.ELEMENT_NODE) {
-                            Element detailElement = (Element) detail;
-                            System.out.println("     " + detailElement.getTagName() + ": " + detailElement.getAttribute("value"));
-                        }
-
-                    }
-
+                
+                NodeList measures = doc.getElementsByTagName("measure");
+                System.out.println("Amount of Measures is: " + measures.getLength());
+                
+                
+                
+                
+                for(int i = 0; i < measures.getLength(); i++) {
+                	
+                	
+                	
                 }
-            }
+                
+                
+        		System.setProperty("ExecutionEnv", "part-list");
+        		String creator = getTestData("score-partwise","score-part",doc);
+                System.out.println(creator);
+	}
+	
+	
+	private static String getTestData(String SectionName, String NodeName, Document doc) 
+	{
+	
 
-
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
+		
+		String env =System.getProperty("ExecutionEnv");
+		//System.out.println("Environment is :" + env );
+		String path ="object/"+SectionName+"/"+env+"/"+NodeName;
+		System.out.println("Path is : " +path );
+		
+		String final_value = null;
+		NodeList nodeList;
+		Node node;
+		XPath xPath;
+		
+		try
+		{
+				xPath= XPathFactory.newInstance().newXPath();
+				nodeList=(NodeList) xPath.compile(path).evaluate(doc, XPathConstants.NODESET);
+		}
+		catch(Exception e)
+		{
+			return null;
+		}
+		
+		for(int temp=0;temp<nodeList.getLength();temp++)
+		{
+			node=nodeList.item(temp);
+			if(node.getNodeName().equals(NodeName))
+			{
+				final_value=node.getTextContent().trim();
+			}
+		}
+		
+		
+		
+		return final_value;
 	}
 	
 	
@@ -75,6 +174,7 @@ import java.io.StringReader;
 	    DocumentBuilder builder = factory.newDocumentBuilder();
 	    InputSource is = new InputSource(new StringReader(xml));
 	    getXml( builder.parse(is) );
+	    
 	    
 	}
 }
