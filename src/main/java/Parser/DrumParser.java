6d954ecc9d9e4c55f069192f1a1b009a8adcdd42
Abdullah-Sawal <90650822+Abdullah-Sawal@users.noreply.github.com>
2022-02-22 14:45:53 -0500

    prints out midi-program, midi-unpitched, volume and pans for drumset(without ids).

--- src/main/java/Parser/DrumParser.java
+++ src/main/java/Parser/DrumParser.java
@@ -12,7 +12,7 @@ public class DrumParser {
 	public void parseDrums(NodeList measures, ArrayList<Integer> nNPM, Document doc) {
 		
 		NodeList instrumentNames =  doc.getElementsByTagName("instrument-name");
-		NodeList midiChannels =  doc.getElementsByTagName("midi-channel");
+		
 		
 		
 		for (int j = 0; j<instrumentNames.getLength(); j++) {
@@ -23,13 +23,42 @@ public class DrumParser {
 
 		}
 		
+		System.out.println("==================================");
 		
+		NodeList midiChannels =  doc.getElementsByTagName("midi-channel");
+		NodeList midiPrograms =  doc.getElementsByTagName("midi-program");
+		NodeList midiUnpitcheds =  doc.getElementsByTagName("midi-unpitched");
+		NodeList volumes =  doc.getElementsByTagName("volume");
+		NodeList pans =  doc.getElementsByTagName("pan");
 
 		for (int k = 0; k<midiChannels.getLength(); k++) {
 
 			Element midiChannel = (Element) midiChannels.item(k);    
 			String  NOC = midiChannel.getTextContent();
-			System.out.println("midi-Channel " + NOC);
+			System.out.println("midi-Channel: " + NOC);
+			
+			Element midiProgram = (Element) midiPrograms.item(k);    
+			String  NOP = midiProgram.getTextContent();
+			System.out.println("midi-Program: " + NOP);
+			
+			Element midiUnpitched = (Element) midiUnpitcheds.item(k);    
+			String  NOU = midiUnpitched.getTextContent();
+			System.out.println("midi-Unpitched: " + NOU);
+			
+			Element volume= (Element) volumes.item(k);    
+			String  NOV = volume.getTextContent();
+			System.out.println("Volume: " + NOV);
+			
+			Element pan = (Element) pans.item(k);    
+			String  NOPA = pan.getTextContent();
+			System.out.println("Pam: " + NOPA);
+			
+			
+			System.out.println("==================================");
+			
+			
+			
+			
 
 		
 
