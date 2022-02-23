430f4e1759acb15493cfe43f75d8f8e38bb4cba6
John Yacoub <jyacoub@my.yorku.ca>
2022-02-23 13:36:47 -0500

    Guitar changes done

--- src/main/java/GUI/PreviewSheetMusicController.java
+++ src/main/java/GUI/PreviewSheetMusicController.java
@@ -41,7 +41,6 @@ public class PreviewSheetMusicController extends Application{
 	public void setMainViewController(MainViewController mvcInput) {
 		mvc = mvcInput;
 		canvasNote.printNotes(canvas);
-
 	}
 
 	@FXML
