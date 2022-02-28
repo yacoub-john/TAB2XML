1d35093e0b301cc096308cda3052552b61f83d09
AKakkar28 <ak28@my.yorku.ca>
2022-02-28 13:23:39 -0500

    drum

---
+++ src/main/java/PlayNotes/JfugueForDrum.java
@@ -0,0 +1,12 @@
+package PlayNotes;
+import org.jfugue.player.Player;
+public class JfugueForDrum {
+
+
+	
+	  public static void main(String[] args) {
+	    Player player1 = new Player();
+	    player1.play("V0 I[HI_MID_TOM] Eq Ch. | Eq Ch. | Dq Eq Dq Cq   V1 I[Flute] Rw | Rw | GmajQQQ CmajQ");
+	  }
+	}
+
