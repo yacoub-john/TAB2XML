d2ab87666acd4052095d81741c6bf8a34a9850b9
AKakkar28 <ak28@my.yorku.ca>
2022-02-28 13:50:06 -0500

    drum

--- src/main/java/PlayNotes/JfugueForDrum.java
+++ src/main/java/PlayNotes/JfugueForDrum.java
@@ -6,7 +6,7 @@ public class JfugueForDrum {
 	
 	  public static void main(String[] args) {
 	    Player player1 = new Player();
-	    player1.play("V0 I[HI_MID_TOM] Eq Ch. | Eq Ch. | Dq Eq Dq Cq   V1 I[Flute] Rw | Rw | GmajQQQ CmajQ");
+	    player1.play("V0 I[HI_MID_TOM] Eq Ch. | Eq Ch. | Dq Eq Dq Cq   V1 I[CLOSED_HI_HAT] Rw | Rw | GmajQQQ CmajQ");
 	  }
 	}
 
