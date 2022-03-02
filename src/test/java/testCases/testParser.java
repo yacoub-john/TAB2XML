2d65cbb08ed77a43590d2b53caaefd22da296cad
Abdullah-Sawal <90650822+Abdullah-Sawal@users.noreply.github.com>
2022-03-02 13:02:47 -0500

    test casses class for xml.

---
+++ src/test/java/testCases/testParser.java
@@ -0,0 +1,29 @@
+package testCases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Parser.XMLParser;
+
+import converter.Converter;
+
+
+class testParser {
+
+	@Test
+	void testXMLParser1() {
+		
+		
+		XMLParser xmlParser = new XMLParser();
+		xmlParser.loadXMLFromString(Converter.getMusicXML());
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
