49f143dd0e465432a7b1f66900628943acf135ed
AKakkar28 <ak28@my.yorku.ca>
2022-03-28 15:22:26 -0400

    save button

--- src/main/java/GUI/PreviewSheetMusicController.java
+++ src/main/java/GUI/PreviewSheetMusicController.java
@@ -13,12 +13,41 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 import utility.Settings;
 import javafx.scene.canvas.Canvas;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+import javafx.application.Application;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.fxml.FXML;
+import javafx.print.PageLayout;
+import javafx.print.PageOrientation;
+import javafx.print.Paper;
+import javafx.print.Printer;
+import javafx.print.PrinterJob;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.transform.Scale;
+import javafx.scene.transform.Translate;
+import javafx.stage.Window;
+import javafx.stage.Stage;
+import models.ScorePartwise;
+import models.measure.Measure;
+
 
 
 public class PreviewSheetMusicController extends Application{
@@ -126,7 +155,38 @@ public class PreviewSheetMusicController extends Application{
 
 	@FXML 
 	void  editInput() {
-		mvc.convertWindow.hide();
+//		mvc.convertWindow.hide();
+	
+		
+
+			WritableImage screenshot = anchor.snapshot(null, null);
+			Printer printer = Printer.getDefaultPrinter();
+			PageLayout layout = printer.createPageLayout(Paper.A4, PageOrientation.PORTRAIT, Printer.MarginType.DEFAULT);
+
+			double pagePrintableWidth = layout.getPrintableWidth();
+			double pagePrintableHeight = layout.getPrintableHeight(); 
+			final double scaleX = pagePrintableWidth / (1.5*screenshot.getWidth());
+			final double scaleY = pagePrintableHeight / screenshot.getHeight();
+			final ImageView print_node = new ImageView(screenshot);
+			print_node.getTransforms().add(new Scale(scaleX, scaleX));
+
+			PrinterJob printSheet = PrinterJob.createPrinterJob();
+
+			if (printSheet != null && printSheet.showPrintDialog(anchor.getScene().getWindow())) {
+
+				double numberOfPages = Math.ceil(scaleX / scaleY);
+				Translate gridTransform = new Translate(0, 0);
+				print_node.getTransforms().add(gridTransform);
+				for (int i = 0; i < numberOfPages; i++) {
+					gridTransform.setY(-i * (pagePrintableHeight / scaleX));
+					printSheet.printPage(layout, print_node);
+				}
+
+				printSheet.endJob();
+
+			}
+
+		
 
 	}
 
